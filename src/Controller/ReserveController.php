1aa66a7a130a9fa14f336a6abaab9e4f0e8f3827
Hamidreza <hamidreza.tahmooresi@gmail.com>
2023-11-21 15:32:26 +0330

    add: routes += list reserve json, composer += serializer

--- src/Controller/ReserveController.php
+++ src/Controller/ReserveController.php
@@ -5,16 +5,23 @@ namespace App\Controller;
 use App\Repository\ReserveRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+
 
 class ReserveController extends AbstractController
 {
-    #[Route('/reserve', name: 'app_reserve')]
-    public function index(ReserveRepository $repo): Response
+    #[Route('/reserve', name: 'app_reserve', methods:['GET'])]
+    public function index(ReserveRepository $repo, SerializerInterface $serializer): JsonResponse
     {
-        return $this->json($repo->findAll());
-//        return $this->render('reserve/index.html.twig', [
-//            'controller_name' => 'ReserveController',
-//        ]);
+
+        $reserves = $repo->findAll();
+        $json = $serializer->serialize($reserves, 'json',);
+        return new JsonResponse(
+            data:$json,
+            status: 200,
+            json:true,
+        );
     }
 }
